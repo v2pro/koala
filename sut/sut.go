72a07e4fee5fcf23bffc2ba8865dfba07e26d7d5
Tao Wen <taowen@gmail.com>
2017-11-07 09:51:48 +0800

    try to fix the socks incorrect issue

--- sut/sut.go
+++ sut/sut.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"syscall"
 )
 
 // InboundRequestPrefix is used to recognize php-fpm FCGI_BEGIN_REQUEST packet.
@@ -31,6 +32,48 @@ func (thread *Thread) lookupSocket(socketFD SocketFD) *socket {
 		sock = getGlobalSock(socketFD)
 		if sock == nil {
 			return nil
+		}
+		remoteAddr, err := syscall.Getpeername(int(socketFD))
+		if err != nil {
+			countlog.Error("event!failed to get peer name", "err", err, "socketFD", socketFD)
+			return nil
+		}
+		remoteAddr4, _ := remoteAddr.(*syscall.SockaddrInet4)
+		// if remote address changed, the fd must be closed and reused
+		if remoteAddr4 != nil && (remoteAddr4.Port != sock.addr.Port ||
+			remoteAddr4.Addr[0] != sock.addr.IP[0] ||
+			remoteAddr4.Addr[1] != sock.addr.IP[1] ||
+			remoteAddr4.Addr[2] != sock.addr.IP[2] ||
+			remoteAddr4.Addr[3] != sock.addr.IP[3]) {
+			sock = &socket{
+				socketFD: socketFD,
+				isServer: false,
+				addr: net.TCPAddr{
+					Port: remoteAddr4.Port,
+					IP:   net.IP(remoteAddr4.Addr[:]),
+				},
+				lastAccessedAt: time.Now(),
+			}
+			setGlobalSock(socketFD, sock)
+		}
+		remoteAddr6, _ := remoteAddr.(*syscall.SockaddrInet6)
+		if remoteAddr6 != nil && (remoteAddr6.Port != sock.addr.Port ||
+			remoteAddr6.Addr[0] != sock.addr.IP[0] ||
+			remoteAddr6.Addr[1] != sock.addr.IP[1] ||
+			remoteAddr6.Addr[2] != sock.addr.IP[2] ||
+			remoteAddr6.Addr[3] != sock.addr.IP[3] ||
+			remoteAddr6.Addr[4] != sock.addr.IP[4] ||
+			remoteAddr6.Addr[5] != sock.addr.IP[5]) {
+			sock = &socket{
+				socketFD: socketFD,
+				isServer: false,
+				addr: net.TCPAddr{
+					Port: remoteAddr6.Port,
+					IP:   net.IP(remoteAddr6.Addr[:]),
+				},
+				lastAccessedAt: time.Now(),
+			}
+			setGlobalSock(socketFD, sock)
 		}
 		thread.socks[socketFD] = sock
 	}
@@ -316,5 +359,6 @@ func (thread *Thread) shutdownRecordingSession() {
 		"threadID", thread.threadID,
 		"sessionId", thread.recordingSession.SessionId)
 	thread.recordingSession.Shutdown(thread)
+	thread.socks = map[SocketFD]*socket{} // socks on thread is a temp cache
 	thread.recordingSession = recording.NewSession(int32(thread.threadID))
 }
