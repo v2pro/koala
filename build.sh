404290d5381834ae30ba6ac3d7b3e1de7bc8de52
Tao Wen <taowen@gmail.com>
2017-11-17 11:38:27 +0800

    build recorder

--- build.sh
+++ build.sh
@@ -13,12 +13,20 @@ cd /tmp/build-golang/src/github.com/v2pro/koala
 
 case $1 in
     "tracer" )
+        # trace & record to file, only for testing purpose
         export CGO_CFLAGS="-DKOALA_LIBC_NETWORK_HOOK -DKOALA_LIBC_FILE_HOOK"
         export CGO_CPPFLAGS=$CGO_CFLAGS
         exec go build -tags="koala_tracer koala_recorder" -buildmode=c-shared -o output/koala-tracer.so github.com/v2pro/koala/cmd/recorder
         ;;
+    "recorder" )
+        # record to file, only for testing purpose
+        export CGO_CFLAGS="-DKOALA_LIBC_NETWORK_HOOK -DKOALA_LIBC_FILE_HOOK"
+        export CGO_CPPFLAGS=$CGO_CFLAGS
+        exec go build -tags="koala_recorder" -buildmode=c-shared -o output/koala-recorder.so github.com/v2pro/koala/cmd/recorder
+        ;;
 esac
 
+# build replayer by default
 export CGO_CFLAGS="-DKOALA_LIBC_NETWORK_HOOK -DKOALA_LIBC_FILE_HOOK -DKOALA_LIBC_TIME_HOOK -DKOALA_LIBC_PATH_HOOK"
 export CGO_CPPFLAGS=$CGO_CFLAGS
 go build -tags="koala_replayer" -buildmode=c-shared -o output/koala-replayer.so github.com/v2pro/koala/cmd/replayer
